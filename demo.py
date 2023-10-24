d2cfe629884b530f0f1d587ef3f9e9b0fdd4d845
Sivaroopan Nirhoshan <nirhosh98@gmail.com>
2023-10-24 22:28:24 +0530

    Add Demo

--- demo.py
+++ demo.py
@@ -1,14 +1,12 @@
+import argparse
 import os
 import json
 import random
-import argparse
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 from torch.optim.lr_scheduler import ReduceLROnPlateau, CosineAnnealingLR
 from tqdm import tqdm
 import sys
@@ -16,19 +14,11 @@ sys.path.append('./')
 from utils import distribute_over_GPUs, validate_arguments
 from model import Model, Identity
 from get_dataloader import get_dataloader
-from sklearn.metrics import f1_score
-from sklearn.linear_model import LogisticRegression
-from sklearn import metrics
-from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, roc_curve,average_precision_score, precision_recall_curve,auc
-import h5py
 from losses import edl_mse_loss, edl_digamma_loss, edl_log_loss, relu_evidence
-
 torch.backends.cudnn.benchmark=True
 class Net(nn.Module):
     def __init__(self, opt):
         super(Net, self).__init__()
-
-        # Load pre-trained model
         base_model = Model(pretrained=opt.pretrained)
         self.f = base_model.f
         self.g1 = base_model.g1
@@ -42,21 +32,9 @@ class Net(nn.Module):
         out = self.fc(feature)
         return out,feature
 
-    
-def plot_roc_curve(true_y, y_prob):
-    """
-    plots the roc curve based of the probabilities
-    """
 
-    fpr, tpr, thresholds = roc_curve(true_y, y_prob)
-    plt.plot(fpr, tpr)
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    
-    
-all_preds, all_labels, all_slides, all_patches  = [], [], [], []
-num_classes=2
 # train or test for one epoch
+all_features=[]
 def train_val(net, data_loader, train_optimizer,device,num_classes,uncertainty=False):
     is_train = train_optimizer is not None
     net.eval() # train only the last layers.
@@ -64,6 +42,7 @@ def train_val(net, data_loader, train_optimizer,device,num_classes,uncertainty=F
 
     total_loss, total_correct, total_num, data_bar = 0.0, 0.0, 0, tqdm(data_loader)
 
+    all_preds, all_labels, all_slides, all_patches  = [], [], [], []
     all_evidence=[]
     all_evidence=torch.FloatTensor(all_evidence)
     all_evidence = all_evidence.cuda(non_blocking=True)
@@ -76,41 +55,26 @@ def train_val(net, data_loader, train_optimizer,device,num_classes,uncertainty=F
                 _, preds = torch.max(out.data, 1)
                 evidence = relu_evidence(out)
                 all_evidence=torch.cat((all_evidence,evidence),dim=0)
-                total_evidence = torch.sum(evidence, 1, keepdim=True)
             else:
                 data = data.cuda(non_blocking=True)
                 out,feature = net(data)
                 _, preds = torch.max(out.data, 1)
+
             if is_train:
                 train_optimizer.zero_grad()
                 train_optimizer.step()
-            _, preds = torch.max(out.data, 1)
 
+            _, preds = torch.max(out.data, 1)
 
             all_preds.extend(preds.cpu().numpy())
             all_labels.extend(target.cpu().data.numpy())
             all_patches.extend(patch_id)
             all_slides.extend(slide_id)
-           # all_features.extend(feature.data.cpu().numpy())
-
-            probs = torch.nn.functional.softmax(out.data, dim=1).cpu().numpy()
-         
-            total_num += data.size(0)
-            prediction = torch.argsort(out, dim=-1, descending=True)
-            total_correct += torch.sum((prediction[:, 0:1] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
-            #F1=f1_score(all_labels, all_preds)
-            #auc1 = metrics.roc_auc_score(all_labels, all_outputs1)
-            #average_precision = average_precision_score(all_labels, all_outputs1)
-           # precision, recall, thresholds = precision_recall_curve(all_labels, all_outputs1)
-           # auc_precision_recall = auc(recall, precision)
-            
-            #data_bar.set_description(f'{"Train" if is_train else "Test"} Epoch: [1]  ACC: {total_correct / total_num * 100:.2f}%  #f1: {F1 * 100:.2f}% AUC:{auc1 * 100:.2f}% PR_AUC1:{average_precision * 100:.2f}% PR_AUC2:{auc_precision_recall * 100:.2f}')
-            data_bar.set_description(f'{"Train" if is_train else "Test"} Epoch: [1] ACC: {total_correct / total_num * 100:.2f}')
-
+            data_bar.set_description(f'{"Train" if is_train else "Test"} Epoch: [1]')
+    
     if uncertainty:     
         alpha = all_evidence + 1
         u = num_classes / torch.sum(alpha, dim=1, keepdim=True)
-        prob = alpha / torch.sum(alpha, dim=1, keepdim=True)
         u=u.cpu().data.numpy()
         u2=u[:, 0]    
         df =  pd.DataFrame({
@@ -126,43 +90,61 @@ def train_val(net, data_loader, train_optimizer,device,num_classes,uncertainty=F
                 'prediction': all_preds,
                 'slide_id': all_slides,
                 'patch_id': all_patches
-        })
-    #return total_correct / total_num * 100, df,F1,auc1,average_precision,auc_precision_recall,precision, recall
-    return total_correct / total_num * 100, df
+                
+            })
+    print("Image 1 - ")
+    if uncertainty:
+        print("Uncertainty score :",u2[0])
+    print("Prediction : ",all_preds[0])
+    print("")
+    print("Image 2 - ")
+    if uncertainty:
+        print("Uncertainty score :",u2[1])
+    print("Prediction : ",all_preds[1])
+    print("")
+    print("Image 3 - ")
+    if uncertainty:
+        print("Uncertainty score :",u2[2])
+    print("Prediction : ",all_preds[2])
+    print("")
+    print("Image 4 - ")
+    if uncertainty:
+        print("Uncertainty score :",u2[3])
+    print("Prediction : ",all_preds[3])
+    print("")
+    return df
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Linear Evaluation')
     group_modelset = parser.add_mutually_exclusive_group(required=True)
-    group_modelset.add_argument('--model_path', type=str, default='results/128_0.5_200_256_200_model_200.pth',
+    group_modelset.add_argument('--model_path', type=str, default="/content/ssl-pathology/simclr/checkpoints/128_0.5_200_256_200_model_200.pth" ,
                         help='The pretrained model path')
-    group_modelset.add_argument('--pre_model_path', type=str, default='logs-a100gpu/128_0.5_128_400_model_400.pth',
-                                help='The pretrained model path')
     group_modelset.add_argument("--random", action="store_true", default=False, help="No pre-training, use random weights")
     group_modelset.add_argument("--pretrained", action="store_true", default=False, help="Use Imagenet pretrained Resnet")
 
-    parser.add_argument('--batch_size', type=int, default=512, help='Number of images in each mini-batch')
-    parser.add_argument('--epochs', type=int, default=20, help='Number of sweeps over the dataset to train')
+    parser.add_argument('--batch_size', type=int, default=128, help='Number of images in each mini-batch')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of sweeps over the dataset to train')
 
-    parser.add_argument('--training_data_csv', required=True, type=str, help='Path to file to use to read training data')
-    parser.add_argument('--test_data_csv', required=True, type=str, help='Path to file to use to read test data')
+    parser.add_argument('--training_data_csv', default="/content/ssl-pathology/mouse_data/mouse_train.csv", required=True, type=str, help='Path to file to use to read training data')
+    parser.add_argument('--test_data_csv', default="/content/ssl-pathology/mouse_data/mouse_test.csv", required=True, type=str, help='Path to file to use to read test data')
     # For validation set, need to specify either csv or train/val split ratio
     group_validationset = parser.add_mutually_exclusive_group(required=True)
     group_validationset.add_argument('--validation_data_csv', type=str, help='Path to file to use to read validation data')
-    group_validationset.add_argument('--trainingset_split', type=float, help='If not none, training csv with be split in train/val. Value between 0-1')
-    parser.add_argument("--num_classes", type=int, default=2, help="Number of classes")
+    group_validationset.add_argument('--trainingset_split', default=0 ,type=float, help='If not none, training csv with be split in train/val. Value between 0-1')
+    parser.add_argument("--num_classes", type=int, default=9, help="Number of classes")
     parser.add_argument("--uncertainty", type=bool, default=False, help="Uncertainty")
-    parser.add_argument('--dataset', choices=['cam', 'patchcam', 'cam_rgb_hed', 'ovary', 'skin'], default='cam', type=str, help='Dataset')
-    parser.add_argument('--data_input_dir', type=str, help='Base folder for images')
+    parser.add_argument('--dataset', choices=['cam', 'patchcam', 'cam_rgb_hed', 'ovary', 'skin','mouse','nct100k'], default='nct100k', type=str, help='Dataset')
+    parser.add_argument('--data_input_dir', default="/content/ssl-pathology/mouse_data" ,type=str, help='Base folder for images')
     parser.add_argument('--data_input_dir_test', type=str, required=False, help='Base folder for images')
-    parser.add_argument('--save_dir', type=str, help='Path to save log')
+    parser.add_argument('--save_dir', default="/content/ssl-pathology/mouse_data/logs", type=str, help='Path to save log')
     parser.add_argument('--save_after', type=int, default=1, help='Save model after every Nth epoch, default every epoch')
     parser.add_argument("--balanced_validation_set", action="store_true", default=False, help="Equal size of classes in validation AND test set",)
 
     parser.add_argument("--finetune", action="store_true", default=False, help="If true, pre-trained model weights will not be frozen.")
     parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
     parser.add_argument('--weight_decay', type=float, default=1e-6, help='Weight decay (l2 reg)')
-    parser.add_argument("--model_to_save", choices=['best', 'latest'], default='latest', type=str, help='Save latest or best (based on val acc)')
+    parser.add_argument("--model_to_save", choices=['best', 'latest'], default='best', type=str, help='Save latest or best (based on val acc)')
     parser.add_argument('--seed', type=int, default=44, help='seed')
 
 
@@ -183,7 +165,6 @@ if __name__ == '__main__':
     parser.add_argument("--rgb_contrast_p", type=float, default=0, help="prob of using contrast (rgb only)")
     parser.add_argument("--rgb_grid_distort_p", type=float, default=0, help="probability of using grid distort (only on rgb)" )
     parser.add_argument("--rgb_grid_shuffle_p", type=float, default=0, help="probability of using grid shuffle (only on rgb)" )
-
 
     opt = validate_arguments(parser.parse_args())
 
@@ -217,7 +198,7 @@ if __name__ == '__main__':
 
     model = Net(opt)
     model, num_GPU = distribute_over_GPUs(opt, model)
-    
+
     train_loader, train_data, val_loader, val_data, test_loader, test_data = get_dataloader(opt)
 
     if not opt.finetune:
@@ -231,17 +212,15 @@ if __name__ == '__main__':
                               momentum=0.9, nesterov=True)
 
     scheduler = CosineAnnealingLR(optimizer, opt.epochs)
-
+    num_classes=opt.num_classes
     criterion = edl_mse_loss
     loss_criterion = nn.CrossEntropyLoss().to(opt.device)
-    results = {'train_acc': [],
-               'val_acc': []}
-   
+    results = {'train_loss': [], 'train_acc': [],
+               'val_loss': [], 'val_acc': [],'F1_score':[]}
+
+    # Load saved model
     model.load_state_dict(torch.load(f'{opt.model_path}'))
     model.eval()
-    #test_acc, df,F1,auc1,average_precision,auc_precision_recall,precision, recall = train_val(model, test_loader, None,opt.device)
-    test_acc, df = train_val(model, test_loader, None,opt.device,num_classes,uncertainty=opt.uncertainty)
-    #all_features=np.asarray(all_features)
 
-    df.to_csv(
-        f"{opt.log_path}/inference_result.csv")
+    df = train_val(model, test_loader, None,opt.device,num_classes,uncertainty=opt.uncertainty)
+    df.to_csv(f"{opt.log_path}/demo_result.csv")
